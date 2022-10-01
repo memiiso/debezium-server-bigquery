3bec80551d501c5ab940a1e53bc7226ffd162058
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2022-10-01 17:05:44 +0200

    Use upstream DebeziumMetrics (#26)
    * Use upstream DebeziumMetrics
    
    * Use upstream DebeziumMetrics
    
    * Use upstream DebeziumMetrics
    
    (cherry picked from commit cc12bc708539f0ad3c0770ed456e4d3d37b922a5)

--- debezium-server-bigquery-sinks/src/test/java/io/debezium/server/bigquery/StreamBigqueryChangeConsumerMysqlTest.java
+++ debezium-server-bigquery-sinks/src/test/java/io/debezium/server/bigquery/StreamBigqueryChangeConsumerMysqlTest.java
@@ -24,7 +24,6 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 /**
- * Integration test that verifies basic reading from PostgreSQL database and writing to s3 destination.
  *
  * @author Ismail Simsek
  */
