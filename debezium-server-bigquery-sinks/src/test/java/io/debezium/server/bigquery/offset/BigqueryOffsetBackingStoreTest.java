a38de09f0dcee7c1ce80b1d285454601e8130ace
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2023-02-02 20:12:46 +0100

    Add storage class to save offset to bq

--- debezium-server-bigquery-sinks/src/test/java/io/debezium/server/bigquery/offset/BigqueryOffsetBackingStoreTest.java
+++ debezium-server-bigquery-sinks/src/test/java/io/debezium/server/bigquery/offset/BigqueryOffsetBackingStoreTest.java
@@ -3,10 +3,14 @@
  *  * Copyright memiiso Authors.
  *  *
  *  * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
- *  
+ *
  */
 
 package io.debezium.server.bigquery.offset;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.QuarkusTestProfile;
+import io.quarkus.test.junit.TestProfile;
 
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
@@ -18,42 +22,49 @@ import java.util.Map;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.connect.runtime.WorkerConfig;
 import org.apache.kafka.connect.util.Callback;
+import org.eclipse.microprofile.config.ConfigProvider;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.jupiter.api.Disabled;
-import static io.debezium.server.bigquery.ConfigSource.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Disabled
+@QuarkusTest
+@TestProfile(BigqueryOffsetBackingStoreTest.TestProfile.class)
 public class BigqueryOffsetBackingStoreTest {
 
-  private final Map<ByteBuffer, ByteBuffer> firstSet = new HashMap<>();
-  private final Map<ByteBuffer, ByteBuffer> secondSet = new HashMap<>();
-  Map<String, String> props = new HashMap<>();
+  private static final Map<ByteBuffer, ByteBuffer> firstSet = new HashMap<>();
+  private static final Map<ByteBuffer, ByteBuffer> secondSet = new HashMap<>();
 
-  public String fromByteBuffer(ByteBuffer data) {
+  public static String fromByteBuffer(ByteBuffer data) {
     return (data != null) ? String.valueOf(StandardCharsets.UTF_16.decode(data.asReadOnlyBuffer())) : null;
   }
 
-  public ByteBuffer toByteBuffer(String data) {
+  public static ByteBuffer toByteBuffer(String data) {
     return (data != null) ? ByteBuffer.wrap(data.getBytes(StandardCharsets.UTF_16)) : null;
   }
 
-  @Before
-  public void setup() {
-    props.put("debezium.sink.type", "bigquerybatch");
-    props.put("debezium.sink.bigquerybatch.project", "test");
-    props.put("debezium.sink.bigquerybatch.dataset", BQ_DATASET);
-    props.put("debezium.sink.bigquerybatch.location", BQ_LOCATION);
-    props.put("debezium.sink.bigquerybatch.credentialsFile", BQ_CRED_FILE);
-    props.put("debezium.source.offset.storage", "io.debezium.server.bigquery.offset.BigqueryOffsetBackingStore");
-    props.put("debezium.source.offset.flush.interval.ms", "60000");
-    props.put("debezium.source.offset.storage.bigquery.table-name", "debezium_offset_storage_custom_table");
-    firstSet.put(this.toByteBuffer("key"), this.toByteBuffer("value"));
-    firstSet.put(this.toByteBuffer("key2"), null);
-    secondSet.put(this.toByteBuffer("key1secondSet"), this.toByteBuffer("value1secondSet"));
-    secondSet.put(this.toByteBuffer("key2secondSet"), this.toByteBuffer("value2secondSet"));
+  @BeforeClass
+  public static void setup() {
+    firstSet.put(toByteBuffer("key"), toByteBuffer("value"));
+    firstSet.put(toByteBuffer("key2"), null);
+    secondSet.put(toByteBuffer("key1secondSet"), toByteBuffer("value1secondSet"));
+    secondSet.put(toByteBuffer("key2secondSet"), toByteBuffer("value2secondSet"));
+  }
+
+  public Map<String, String> config() {
+    Map<String, String> conf = new HashMap<>();
+    for (String propName : ConfigProvider.getConfig().getPropertyNames()) {
+      if (propName.startsWith("debezium")) {
+        try {
+          conf.put(propName, ConfigProvider.getConfig().getValue(propName, String.class));
+        } catch (Exception e) {
+          conf.put(propName, "");
+        }
+      }
+    }
+    return conf;
   }
 
   @Test
@@ -61,7 +72,7 @@ public class BigqueryOffsetBackingStoreTest {
     // multiple initialization should not fail
     // first one should create the table and following ones should use the created table
     BigqueryOffsetBackingStore store = new BigqueryOffsetBackingStore();
-    store.configure(new TestWorkerConfig(props));
+    store.configure(new TestWorkerConfig(config()));
     store.start();
     store.start();
     store.start();
@@ -74,13 +85,13 @@ public class BigqueryOffsetBackingStoreTest {
     };
 
     BigqueryOffsetBackingStore store = new BigqueryOffsetBackingStore();
-    store.configure(new TestWorkerConfig(props));
+    store.configure(new TestWorkerConfig(config()));
     store.start();
     store.set(firstSet, cb).get();
 
-    Map<ByteBuffer, ByteBuffer> values = store.get(Arrays.asList(this.toByteBuffer("key"), this.toByteBuffer("bad"))).get();
-    assertEquals(this.toByteBuffer("value"), values.get(this.toByteBuffer("key")));
-    Assert.assertNull(values.get(this.toByteBuffer("bad")));
+    Map<ByteBuffer, ByteBuffer> values = store.get(Arrays.asList(toByteBuffer("key"), toByteBuffer("bad"))).get();
+    assertEquals(toByteBuffer("value"), values.get(toByteBuffer("key")));
+    Assert.assertNull(values.get(toByteBuffer("bad")));
   }
 
   @Test
@@ -89,26 +100,38 @@ public class BigqueryOffsetBackingStoreTest {
     };
 
     BigqueryOffsetBackingStore store = new BigqueryOffsetBackingStore();
-    store.configure(new TestWorkerConfig(props));
+    store.configure(new TestWorkerConfig(config()));
     store.start();
     store.set(firstSet, cb).get();
     store.set(secondSet, cb).get();
     store.stop();
     // Restore into a new store mand make sure its correctly reload
     BigqueryOffsetBackingStore restore = new BigqueryOffsetBackingStore();
-    restore.configure(new TestWorkerConfig(props));
+    restore.configure(new TestWorkerConfig(config()));
     restore.start();
-    Map<ByteBuffer, ByteBuffer> values = restore.get(Collections.singletonList(this.toByteBuffer("key"))).get();
-    Map<ByteBuffer, ByteBuffer> values2 = restore.get(Collections.singletonList(this.toByteBuffer("key1secondSet"))).get();
-    Map<ByteBuffer, ByteBuffer> values3 = restore.get(Collections.singletonList(this.toByteBuffer("key2secondSet"))).get();
-    assertEquals(this.toByteBuffer("value"), values.get(this.toByteBuffer("key")));
-    assertEquals(this.toByteBuffer("value1secondSet"), values2.get(this.toByteBuffer("key1secondSet")));
-    assertEquals(this.toByteBuffer("value2secondSet"), values3.get(this.toByteBuffer("key2secondSet")));
+    Map<ByteBuffer, ByteBuffer> values = restore.get(Collections.singletonList(toByteBuffer("key"))).get();
+    Map<ByteBuffer, ByteBuffer> values2 = restore.get(Collections.singletonList(toByteBuffer("key1secondSet"))).get();
+    Map<ByteBuffer, ByteBuffer> values3 = restore.get(Collections.singletonList(toByteBuffer("key2secondSet"))).get();
+    assertEquals(toByteBuffer("value"), values.get(toByteBuffer("key")));
+    assertEquals(toByteBuffer("value1secondSet"), values2.get(toByteBuffer("key1secondSet")));
+    assertEquals(toByteBuffer("value2secondSet"), values3.get(toByteBuffer("key2secondSet")));
   }
 
   public static class TestWorkerConfig extends WorkerConfig {
     public TestWorkerConfig(Map<String, String> props) {
       super(new ConfigDef(), props);
+    }
+  }
+
+  public static class TestProfile implements QuarkusTestProfile {
+    @Override
+    public Map<String, String> getConfigOverrides() {
+      Map<String, String> config = new HashMap<>();
+      config.put("debezium.sink.type", "bigquerybatch");
+      config.put("debezium.source.offset.storage", "io.debezium.server.bigquery.offset.BigqueryOffsetBackingStore");
+      config.put("debezium.source.offset.flush.interval.ms", "60000");
+      config.put("debezium.source.offset.storage.bigquery.table-name", "debezium_offset_storage_custom_table");
+      return config;
     }
   }
 }
