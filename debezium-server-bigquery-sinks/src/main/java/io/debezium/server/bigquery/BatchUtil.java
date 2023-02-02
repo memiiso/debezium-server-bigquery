93798bd831bcd7aee2734e471dcbd5383578ef75
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2023-02-02 19:00:35 +0100

    improve initializing bqClient (#73)

--- debezium-server-bigquery-sinks/src/main/java/io/debezium/server/bigquery/BatchUtil.java
+++ debezium-server-bigquery-sinks/src/main/java/io/debezium/server/bigquery/BatchUtil.java
@@ -10,11 +10,18 @@ package io.debezium.server.bigquery;
 
 import io.debezium.DebeziumException;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.literal.NamedLiteral;
 
+import com.google.api.gax.retrying.RetrySettings;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
 import org.eclipse.microprofile.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -52,4 +59,48 @@ public class BatchUtil {
     return instance.get();
   }
 
+  public static BigQuery getBQClient(Optional<String> gcpProject, Optional<String> bqDataset, Optional<String> credentialsFile, String bqLocation) throws InterruptedException {
+
+    if (gcpProject.isEmpty()) {
+      throw new InterruptedException("Please provide a value for `debezium.sink.{bigquerybatch|bigquerystream}.project`");
+    }
+
+    if (bqDataset.isEmpty()) {
+      throw new InterruptedException("Please provide a value for `debezium.sink.{bigquerybatch|bigquerystream}.dataset`");
+    }
+
+    GoogleCredentials credentials;
+    try {
+      if (credentialsFile.isPresent() && !credentialsFile.orElse("").isEmpty()) {
+        credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsFile.get()));
+      } else {
+        credentials = GoogleCredentials.getApplicationDefault();
+      }
+    } catch (IOException e) {
+      throw new DebeziumException("Failed to initialize google credentials", e);
+    }
+
+    return BigQueryOptions.newBuilder()
+        .setCredentials(credentials)
+        .setProjectId(gcpProject.get())
+        .setLocation(bqLocation)
+        .setRetrySettings(
+            RetrySettings.newBuilder()
+                // Set the max number of attempts
+                .setMaxAttempts(5)
+                // InitialRetryDelay controls the delay before the first retry. 
+                // Subsequent retries will use this value adjusted according to the RetryDelayMultiplier. 
+                .setInitialRetryDelay(org.threeten.bp.Duration.ofSeconds(5))
+                .setMaxRetryDelay(org.threeten.bp.Duration.ofSeconds(60))
+                // Set the backoff multiplier
+                .setRetryDelayMultiplier(2.0)
+                // Set the max duration of all attempts
+                .setTotalTimeout(org.threeten.bp.Duration.ofMinutes(5))
+                .build()
+        )
+        .build()
+        .getService();
+    
+  }
+  
 }
