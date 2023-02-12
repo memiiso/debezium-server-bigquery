f7b5d60585d4bb08aef9d99e0e8cfe6d93cca48d
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2023-02-12 17:20:02 +0100

    Improve testing code and dependencies (#80)
    * Improve testing code
    
    * Improve testing code
    
    * Improve testing code

--- debezium-server-bigquery-sinks/src/test/java/io/debezium/server/bigquery/history/BigquerySchemaHistoryTest.java
+++ debezium-server-bigquery-sinks/src/test/java/io/debezium/server/bigquery/history/BigquerySchemaHistoryTest.java
@@ -23,7 +23,7 @@ import java.sql.Types;
 import java.util.Map;
 
 import org.junit.jupiter.api.*;
-import static io.debezium.server.bigquery.ConfigSource.*;
+import static io.debezium.server.bigquery.TestConfigSource.*;
 
 
 @Disabled
