b94d7e9cea07fd3213d2ccd2417ce77dc086b1b8
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2023-04-01 18:03:39 +0200

    Optimize streaming code

--- debezium-server-bigquery-sinks/src/main/java/io/debezium/server/bigquery/DebeziumBigqueryEvent.java
+++ debezium-server-bigquery-sinks/src/main/java/io/debezium/server/bigquery/DebeziumBigqueryEvent.java
@@ -32,30 +32,6 @@ import org.slf4j.LoggerFactory;
  */
 public class DebeziumBigqueryEvent {
   protected static final Logger LOGGER = LoggerFactory.getLogger(DebeziumBigqueryEvent.class);
-
-  private static final ImmutableMap<Field.Mode, TableFieldSchema.Mode> BQTableSchemaModeMap =
-      ImmutableMap.of(
-          Field.Mode.NULLABLE, TableFieldSchema.Mode.NULLABLE,
-          Field.Mode.REPEATED, TableFieldSchema.Mode.REPEATED,
-          Field.Mode.REQUIRED, TableFieldSchema.Mode.REQUIRED);
-
-  private static final ImmutableMap<StandardSQLTypeName, TableFieldSchema.Type> BQTableSchemaTypeMap =
-      new ImmutableMap.Builder<StandardSQLTypeName, TableFieldSchema.Type>()
-          .put(StandardSQLTypeName.BOOL, TableFieldSchema.Type.BOOL)
-          .put(StandardSQLTypeName.BYTES, TableFieldSchema.Type.BYTES)
-          .put(StandardSQLTypeName.DATE, TableFieldSchema.Type.DATE)
-          .put(StandardSQLTypeName.DATETIME, TableFieldSchema.Type.DATETIME)
-          .put(StandardSQLTypeName.FLOAT64, TableFieldSchema.Type.DOUBLE)
-          .put(StandardSQLTypeName.GEOGRAPHY, TableFieldSchema.Type.GEOGRAPHY)
-          .put(StandardSQLTypeName.INT64, TableFieldSchema.Type.INT64)
-          .put(StandardSQLTypeName.NUMERIC, TableFieldSchema.Type.NUMERIC)
-          .put(StandardSQLTypeName.STRING, TableFieldSchema.Type.STRING)
-          .put(StandardSQLTypeName.STRUCT, TableFieldSchema.Type.STRUCT)
-          .put(StandardSQLTypeName.TIME, TableFieldSchema.Type.TIME)
-          .put(StandardSQLTypeName.TIMESTAMP, TableFieldSchema.Type.TIMESTAMP)
-          .put(StandardSQLTypeName.JSON, TableFieldSchema.Type.JSON)
-          .build();
-
   public static final List<String> TS_MS_FIELDS = List.of("__ts_ms", "__source_ts_ms");
   public static final List<String> BOOLEAN_FIELDS = List.of("__deleted");
   protected static final ObjectMapper mapper = new ObjectMapper();
@@ -204,40 +180,6 @@ public class DebeziumBigqueryEvent {
 
     clusteringFields.add(clusteringField);
     return Clustering.newBuilder().setFields(clusteringFields).build();
-  }
-
-  public static TableSchema convertBigQuerySchema2TableSchema(Schema schema) {
-
-    TableSchema.Builder result = TableSchema.newBuilder();
-    for (int i = 0; i < schema.getFields().size(); i++) {
-      result.addFields(i, convertFieldSchema(schema.getFields().get(i)));
-    }
-    return result.build();
-  }
-
-  /**
-   * Converts from bigquery v2 Field Schema to bigquery storage API Field Schema.
-   *
-   * @param field the BigQuery client Field Schema
-   * @return the bigquery storage API Field Schema
-   */
-  private static TableFieldSchema convertFieldSchema(Field field) {
-    TableFieldSchema.Builder result = TableFieldSchema.newBuilder();
-    if (field.getMode() == null) {
-      field = field.toBuilder().setMode(Field.Mode.NULLABLE).build();
-    }
-    result.setMode(BQTableSchemaModeMap.get(field.getMode()));
-    result.setName(field.getName());
-    result.setType(BQTableSchemaTypeMap.get(field.getType().getStandardType()));
-    if (field.getDescription() != null) {
-      result.setDescription(field.getDescription());
-    }
-    if (field.getSubFields() != null) {
-      for (int i = 0; i < field.getSubFields().size(); i++) {
-        result.addFields(i, convertFieldSchema(field.getSubFields().get(i)));
-      }
-    }
-    return result.build();
   }
 
   public String destination() {
