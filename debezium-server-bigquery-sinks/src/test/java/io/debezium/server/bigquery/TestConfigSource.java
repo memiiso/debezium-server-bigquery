df32cf8f7295cc9a9c2333e194048cc67dfe4fc8
Ismail Simsek <6005685+ismailsimsek@users.noreply.github.com>
2023-05-11 18:27:06 +0200

    Improve BigqueryOffsetBackingStore and tests

--- debezium-server-bigquery-sinks/src/test/java/io/debezium/server/bigquery/TestConfigSource.java
+++ debezium-server-bigquery-sinks/src/test/java/io/debezium/server/bigquery/TestConfigSource.java
@@ -21,6 +21,8 @@ public class TestConfigSource implements ConfigSource {
   public static String BQ_PROJECT = "test";
   public static String BQ_DATASET = "stage";
   public static String BQ_CRED_FILE = ""; // "/path/to/application_credentials.json"
+  public static String OFFSET_TABLE = "__debezium_offset_storage_test_table";
+  public static String HISTORY_TABLE = "__debezium_database_history_storage_test_table";
   public static List<String> TABLES = List.of("customers", "geom", "orders", "products", "products_on_hand",
       "test_data_types", "test_table");
   protected Map<String, String> config = new HashMap<>();
@@ -31,15 +33,18 @@ public class TestConfigSource implements ConfigSource {
     config.put("debezium.source.decimal.handling.mode", "double");
     config.put("debezium.source.max.batch.size", "100");
     config.put("debezium.source.poll.interval.ms", "5000");
-    config.put("debezium.source.database.server.name", "testc");
+    //config.put("debezium.source.database.server.name", "testc");
+    config.put("debezium.source.database.server.id", "1234");
+    config.put("debezium.source.topic.prefix", "testc");
     //
-    config.put("debezium.source.offset.storage", "org.apache.kafka.connect.storage.MemoryOffsetBackingStore");
-    //config.put("debezium.source.offset.storage", "io.debezium.server.bigquery.offset.BigqueryOffsetBackingStore");
+    //config.put("debezium.source.offset.storage", "org.apache.kafka.connect.storage.MemoryOffsetBackingStore");
+    config.put("debezium.source.offset.storage", "io.debezium.server.bigquery.offset.BigqueryOffsetBackingStore");
     config.put("debezium.source.offset.storage.bigquery.table-name", "__debezium_offset_storage_test_table");
     config.put("debezium.source.offset.flush.interval.ms", "60000");
-    config.put("debezium.source.database.history.kafka.bootstrap.servers", "kafka:9092");
-    config.put("debezium.source.database.history.kafka.topic", "dbhistory.fullfillment");
-    config.put("debezium.source.database.history", "io.debezium.relational.history.MemoryDatabaseHistory");
+    config.put("debezium.source.schema.history.internal", "io.debezium.server.bigquery.history.BigquerySchemaHistory");
+    config.put("debezium.source.schema.history.internal.bigquery.table-name", 
+        "__debezium_database_history_storage_test_table");
+    //config.put("debezium.source.schema.history.internal", "io.debezium.relational.history.MemoryDatabaseHistory");
     //
     config.put("debezium.source.table.include.list", "inventory.*");
     config.put("debezium.source.snapshot.select.statement.overrides.inventory.products_on_hand", "SELECT * FROM products_on_hand WHERE 1>2");
